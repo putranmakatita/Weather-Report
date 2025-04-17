1e748664d09df3eb61f4c2304225598e2d1748d2
Git <lina@rcelectronic.co.id>
2025-04-17 13:31:44 +0700

    fix logic get report

--- main.js
+++ main.js
@@ -1,36 +1,33 @@
 const buttonSearch = document.querySelector(".button-search");
-let weather= {
-    apiKey : "e32a40a32fa847c0e46e25fe3123e1bd",
-    fetchWeather : function (city) {
+let weather = {
+    apiKey: "e32a40a32fa847c0e46e25fe3123e1bd",
+    fetchWeather: function (city) {
         fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q="
-    + city + "&APPID=" + this.apiKey + "&units=metric"
-    ).then((response) => response.json())
-    .then((data) => this.displayWeather(data))
-    .catch((message) => alert("Name of the city is not found"));
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city + "&APPID=" + this.apiKey + "&units=metric"
+        ).then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+            .catch((message) => alert("Kota tidak ditemukan"));
     },
-    displayWeather : function (data){
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
         document.querySelector(".city").innerText = name;
         document.querySelector(".weather").innerText = description;
-        document.querySelector(".icon").src = 'https://openweathermap.org/img/wn/'+ icon +'.png'
+        document.querySelector(".icon").src = 'https://openweathermap.org/img/wn/' + icon + '.png'
         document.querySelector(".temperature").innerText = temp + "°C"
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind-speed").innerText = "Wind speed: " + speed + "km/h";
-        console.log(description);
-        if (description.includes("sky")){
+        if (description.includes("sky")) {
 
             document.querySelector(".background").style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2017/11/04/08/14/tree-2916763_960_720.jpg")';
 
         }
-        else {
-            ;
-        }
+        document.querySelector('.icon').style.display = 'block';
     },
-    search : function() {
+    search: function () {
         this.fetchWeather(document.querySelector(".textbox").value);
         document.querySelector(".textbox").value = ""
 
@@ -38,6 +35,6 @@ let weather= {
     }
 }
 
-buttonSearch.addEventListener("click", function() {
+buttonSearch.addEventListener("click", function () {
     weather.search();
 })
